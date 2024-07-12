901e216a7541fc85a9c21842e7d9431be964a286
RealTesakDev <164282499+RealTesakDev@users.noreply.github.com>
2024-07-12 12:53:27 -0700

    Update mutiscript.lua

--- project delta/mutiscript.lua
+++ project delta/mutiscript.lua
@@ -1,1 +1,2 @@
-
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dementiaenjoyer/homohack/main/loader.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
